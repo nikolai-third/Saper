fb756ca655a7100b6ffbd37075fda56997db222c
nikolai-third <k.pobedin2004@gmail.com>
2022-12-19 19:24:28 +0300

    Remove from nfa_2022_Pobedin/tree/main/Saper

---
+++ Menu.py
@@ -0,0 +1,298 @@
+import pygame
+import random
+import os
+from Square import Square
+from Status_bar import StatusBar
+
+menu_width_height = 300
+button_width = 200
+button_height = 20
+textfield_width = 66
+textfield_height = 50
+square_size = 50
+
+
+class Menu(pygame.sprite.Sprite):
+    def __init__(self, width, height, FPS1, field1, field2, field3, but1, imgsL1, XY1, all_sprites1, kl1,
+                 bombs1, num1, options1, screen1, img, un_img):
+        """
+        :param width: ширина
+        :param height: высота
+        :param FPS1: кадры в секунду
+        :param field1: текстовое поле 1
+        :param field2: текстовое поле 2
+        :param field3: текстовое поле 3
+        :param but1: кнопка
+        :param imgsL1: изображение клеток
+        :param XY1: массив для координат клеток с цифрами
+        :param all_sprites1: группы для всех спрайтов
+        :param kl1: словарь для клеток с цифрами
+        :param bombs1: слоаварь для клток с бомбами
+        :param num1: массив для координат клеток с бомбами
+        :param options1: группа спрайтов для меню
+        :param screen1: экран
+        :param img: изображение меню
+        """
+
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((300, 300))
+        self.imgsL = imgsL1
+        self.un_images = un_img
+        self.images = img
+        self.image = self.images[0]
+        self.rect = self.image.get_rect()
+        self.rect.x = (width-300)//2
+        self.rect.y = (height-300)//4
+        self.clock = pygame.time.Clock()
+        self.FPS = FPS1
+        self.field1 = field1
+        self.field2 = field2
+        self.field3 = field3
+        self.but1 = but1
+        self.XY = XY1
+        self.all_sprites = all_sprites1
+        self.kl = kl1
+        self.bombs = bombs1
+        self.num = num1
+        self.options = options1
+        self.screen = screen1
+        self.size = 50
+
+    def render(self, win_or_lose_marker):
+        self.image = self.images[win_or_lose_marker]
+        """
+        win_or_lose_marker == 0 - нейтральный режим - первый спрайт в массиве
+        win_or_lose_marker == 1 - режим победы - второй спрайт в массиве
+        win_or_lose_marker == -1 -режим проигрыша - последний спрайт в массиве
+        """
+
+        run = True
+        while run:
+            self.clock.tick(self.FPS)
+
+            for eventMenu in pygame.event.get():
+                if eventMenu.type == pygame.QUIT:
+                    run = False
+
+                if eventMenu.type == pygame.KEYUP:
+                    if eventMenu.key == pygame.K_ESCAPE:
+                        run = False
+
+                if eventMenu.type == pygame.KEYDOWN:
+                    """
+                    Если пользователь нажимает на цифры, то проверяется активно ли какое-нибудь текстовое поле, если да
+                    то в него надо ввести набранную цифру, если пользователь нажал на backsapce, то нужно удалить одну
+                    цифру
+                    """
+
+                    if 48 <= eventMenu.key <= 57:
+                        if self.field1.station == 1:
+                            self.field1.texts('{}'.format(eventMenu.key-48))
+
+                        elif self.field2.station == 1:
+                            self.field2.texts('{}'.format(eventMenu.key - 48))
+
+                        elif self.field3.station == 1:
+                            self.field3.texts('{}'.format(eventMenu.key - 48))
+
+                    if eventMenu.key == pygame.K_BACKSPACE:
+                        self.field1.texts('-1')
+                        self.field2.texts('-1')
+                        self.field3.texts('-1')
+
+                    if eventMenu.key == pygame.K_RETURN:
+                        if check(self.field1.text, self.field2.text, self.field3.text):
+                            restart(self.XY, self.all_sprites, self.kl, self.bombs, self.num,
+                                    int(self.field1.text) * self.size + 1, int(self.field2.text) * self.size + 1,
+                                    self.imgsL, self.size, int(self.field3.text))
+
+                            between_textfields_x = 20
+                            textfield_y = 90  # координаты текстового поля по у в системе отсчета меню
+                            self.rect.x = (int(self.field1.text) * self.size + 1 - menu_width_height) // 2  # меню в центре окна
+                            self.rect.y = (int(self.field2.text) * self.size + 1 - menu_width_height) // 4
+
+                            self.field1.rect.x = self.rect.x + 31  # Новые координаты для всех элементов меню
+                            self.field1.rect.y = self.rect.y + textfield_y
+
+                            self.field2.rect.x = self.field1.rect.x + textfield_width + between_textfields_x
+                            self.field2.rect.y = self.rect.y + textfield_y
+
+                            self.field3.rect.x = self.field2.rect.x + textfield_width + between_textfields_x
+                            self.field3.rect.y = self.rect.y + textfield_y
+
+                            self.but1.rect.x = self.rect.x + 50
+                            self.but1.rect.y = self.rect.y + 220
+
+                            run = False  # Меню убирается
+
+                if eventMenu.type == pygame.MOUSEBUTTONDOWN:
+                    if eventMenu.button == 1:
+                        if self.but1.click(1) == 1:  # Если нажата кнопка, то игра перезапускается
+                            if check(self.field1.text, self.field2.text, self.field3.text):  # проверяет корректность введенных данных
+                                restart(self.XY, self.all_sprites, self.kl, self.bombs, self.num,
+                                        int(self.field1.text) * self.size + 1, int(self.field2.text) * self.size + 1,
+                                        self.imgsL, self.size, int(self.field3.text))
+
+                                between_textfields_x = 20
+                                textfield_y = 90  # координаты текстового поля по у в системе отсчета меню
+
+                                self.rect.x = (int(self.field1.text) * self.size + 1 - menu_width_height) // 2  # меню в центре окна
+                                self.rect.y = (int(self.field2.text) * self.size + 1 - menu_width_height) // 4
+
+                                self.field1.rect.x = self.rect.x + 31  # Новые координаты для всех элементов меню
+                                self.field1.rect.y = self.rect.y + textfield_y
+
+                                self.field2.rect.x = self.field1.rect.x + textfield_width + between_textfields_x
+                                self.field2.rect.y = self.rect.y + textfield_y
+
+                                self.field3.rect.x = self.field2.rect.x + textfield_width + between_textfields_x
+                                self.field3.rect.y = self.rect.y + textfield_y
+
+                                self.but1.rect.x = self.rect.x + 50
+                                self.but1.rect.y = self.rect.y + 220
+
+                                run = False  # Меню убирается
+
+                            else:
+                                self.image = self.un_images[win_or_lose_marker]  # Нужно заменить изображение на
+                                # изображение с надписью о том что нельзя создать такое поле
+
+                        self.field1.state()
+                        self.field2.state()
+                        self.field3.state()
+
+            self.but1.click(0)  # Проверяем не навел ли игрок курсор на кнопку
+            self.options.update()
+            self.options.draw(self.screen)
+            pygame.display.flip()
+
+
+def check(field1_txt, field2_txt, field3_txt):
+    """
+    фунцкция проверяет можно ли по введенным пользователем значениям в текстовые поля созздать игровое поле
+    :return:
+    """
+
+    equation = [field1_txt, field2_txt, field3_txt]
+    if '' in equation:  # Если одно из полей пустое, то нельзя
+        return False
+
+    for i in range(3):
+        equation[i] = int(equation[i])
+
+    if equation[0] * equation[1] - 8 <= equation[2]:  # Если бомб больше, чем клеток, то нельзя
+        return False
+
+    if equation[0] * equation[1] - equation[2] > 1500:  # Если бомб слишком мало, то нельзя
+        return False
+
+    if equation[0] < 6 or equation[1] < 6:  # Размер меню 6х6 клеток, так что меньше нельзя
+        return False
+
+    return True
+
+
+def restart(XY1, all_sprites1, kl1, bombs1, num1, w, h, imgsL1, size, goal):
+    """
+    Функция, которая перезапускает игру, когда игрок нажимает на кнопку. Создает поле нужного размера со всеми пустыми
+    кнопками
+    :param XY1: массив с координатами клеток с цифрами
+    :param all_sprites1: группа спрайтов для всех спрайтов
+    :param kl1: словарь с клетками
+    :param bombs1: словарь с клетками с бомбами
+    :param num1: массив с координатами клеток с бомбами
+    :param w: ширина окна
+    :param h: высота окна
+    :param imgsL1: массив с изображениями
+    :param size: размер меню
+    :return:
+    """
+
+    game_folder = os.path.dirname(os.path.abspath(__file__))
+    img_folder = os.path.join(game_folder, 'img')
+    f2 = pygame.font.Font('/{}/digitalicg.ttf'.format(img_folder), 35)  # Шрифт для статус бара
+
+    pygame.display.set_mode((w, h + 49))
+    pygame.display.flip()
+    bombs1.clear()
+    XY1.clear()
+    kl1.clear()
+    all_sprites1.empty()
+    num1.clear()
+
+    for i in range(w // size):
+        for j in range(h // size):
+            XY1.append([i * size + 1, j * size + 1])
+            m = [i * size + 1, j * size + 1]
+            square = (Square(i * size + 1, j * size + 1, 0, 0, imgsL1, kl1, bombs1, size))
+            kl1[str(m)] = square
+            all_sprites1.add(square)
+
+    status_bar = StatusBar(w, h, 0, goal, pygame.time.get_ticks(), f2)
+    all_sprites1.add(status_bar)
+    kl1['SB'] = status_bar
+
+
+def touch(m, size1):
+    """
+    выдает координаты клеток, которые соприкасаются с данной
+    :param m:
+    :param size1:
+    :return:
+    """
+
+    th = [[m[0], m[1] - size1], [m[0] + size1, m[1] - size1], [m[0] + size1, m[1]], [m[0] + size1, m[1] + size1],
+          [m[0], m[1] + size1], [m[0] - size1, m[1] + size1], [m[0] - size1, m[1]], [m[0] - size1, m[1] - size1]]
+
+    return th
+
+
+def first_click(x0, y0, XY1, all_sprites1, kl1, bombs1, num1, w, h, imgsL1, number1, size):
+    """
+    Функция вызывается после первого клика, генерирует поле, при этом первая клетка всегда должна быть пустой
+    :param x0: х первого клика
+    :param y0: у первого клика
+    :param XY1: массив со всеми клетками с цифрами
+    :param all_sprites1: группа спрайтов для всех клеток
+    :param kl1: словарь со всеми клетками: ключ - координаты, значение - спрайт
+    :param bombs1: словарь с бомбами
+    :param num1: массив с координатами бомб
+    :param w: ширина экрана
+    :param h: высота экрана
+    :param imgsL1: массив с изображениями
+    :param number1: количество бомб, которые нужно создать
+    :param size: размер клетки
+    :return:
+    """
+    forbidden = []
+    forbidden.append([x0, y0])  # чтобы первая была пустая бомб не должно быть в ней и вокруг неё
+
+    for i in touch([x0, y0], size):
+        forbidden.append(i)
+
+    for i in range(number1):  # случайным образом расставляет бомбы, так чтобы в forbidden координатах их не было
+        m = random.choice(XY1)
+        while m in forbidden:
+            m = random.choice(XY1)
+        num1.append(m)
+        XY1.remove(m)
+
+    for i in range(w // size):  # делает квадраты бомбами или клетками с цифрами
+        for j in range(h // size):
+            m = [i * size + 1, j * size + 1]
+            if m in num1:
+                square = (Square(i * size + 1, j * size + 1, -1, 0, imgsL1, kl1, bombs1, size))
+                kl1[str(m)] = square
+                bombs1[str(m)] = square
+                all_sprites1.add(square)
+            else:
+                Th = touch(m, size)
+                NB = 0  # число, которое будет написано на клетке
+                for u in Th:
+                    if u in num1:
+                        NB += 1
+                square = (Square(i * size + 1, j * size + 1, NB, 0, imgsL1, kl1, bombs1, size))
+                kl1[str(m)] = square
+                all_sprites1.add(square)
+
+
---
+++ main.py
@@ -0,0 +1,194 @@
+import pygame
+import os
+from Menu import Menu
+from Menu import first_click
+from Button import Button
+from TextField import TextField
+from Status_bar import StatusBar
+from Start import StartText
+
+
+FPS = 30
+
+# Задаем цвета
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+x = 500
+y = 50
+os.environ['SDL_VIDEO_WINDOW_POS'] = '%d,%d' % (x, y)
+
+# Создаем игру и окно
+pygame.init()
+
+clock = pygame.time.Clock()
+# variable of time
+
+sizes = pygame.display.Info()
+ScreenWidth = sizes.current_w
+ScreenHeight = sizes.current_h
+# screen size
+
+WIDTH = 300
+HEIGHT = 300
+# window size
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Сапер")
+# create window
+
+
+all_sprites = pygame.sprite.Group()
+
+
+num = []  # Координаты верхних левых углов клеток с бомбами
+XY = []  # Координты верхних левых углов клеток с цифрами
+bombs = {}  # Словарь для взаимодейсвия с бомбами
+kl = {}  # Словарь для взаимодействия с клетками
+
+names_squareL = ['zeroL.png', 'oneL.png', 'twoL.png', 'threeL.png', 'fourL.png', 'fiveL.png', 'sixL.png', 'sevenL.png',
+                 'eightL.png', 'closeL.png', 'flagL.png', 'bombL.png', 'detected_bombL.png', 'exploding_bombL.png']
+
+names_menu = ['menu.png', 'button1.png', 'button2.png', 'TextField.png', 'TextFieldActive.png', 'TextField.png',
+              'TextFieldActive.png', 'TextField.png', 'TextFieldActive.png', 'win_menu.png', 'lose_menu.png',
+              'un_menu.png', 'un_win_menu.png', 'un_lose_menu.png', 'start_text.png']
+
+imgsL = []  # Массив для больших изображений клеток, бомб и флага
+imgs_menu = []  # Массив для изображений меню, текстовых полей и кнопки
+
+game_folder = os.path.dirname(os.path.abspath(__file__))
+img_folder = os.path.join(game_folder, 'img')
+
+menu_width_height = 300
+button_width = 200
+button_height = 50
+textfield_width = 66
+textfield_height = 50
+square_size = 50
+
+start_button = Button(button_width, button_height, 200, 530, pygame.image.load(os.path.join(img_folder, 'start_button1.png')),
+                      pygame.image.load(os.path.join(img_folder, 'start_button2.png')))
+st = StartText(pygame.image.load(os.path.join(img_folder, 'start_text.png')), screen, start_button)
+
+st.render()
+
+for i in range(len(names_squareL)):
+    imgsL.append(pygame.image.load(os.path.join(img_folder, names_squareL[i])))
+
+for i in range(len(names_menu)):
+    imgs_menu.append(pygame.image.load(os.path.join(img_folder, names_menu[i])))
+
+f1 = pygame.font.Font('/{}/ofont.ru_Times New Roman.ttf'.format(img_folder), 36)  # Шрифт для цифр в текстовых полях
+f2 = pygame.font.Font('/{}/digitalicg.ttf'.format(img_folder), 35)  # Шрифт для статус бара
+
+options = pygame.sprite.Group()  # Группа спрайтов, связанных с меню (настройками)
+
+
+but1 = Button(button_width, button_height, (WIDTH - menu_width_height) // 2 + 50,
+              (HEIGHT - menu_width_height) // 4 + 220, imgs_menu[1], imgs_menu[2])
+
+field1 = TextField(textfield_width, textfield_height, (WIDTH - menu_width_height) // 2 + 31,
+                   (HEIGHT - menu_width_height)//4 + 90, imgs_menu[3], imgs_menu[4], f1, '15')
+
+field2 = TextField(textfield_width, textfield_height, field1.rect.x + 66 + 20, (HEIGHT - menu_width_height) // 4 + 90,
+                   imgs_menu[5], imgs_menu[6], f1, '15')
+
+field3 = TextField(textfield_width, textfield_height, field2.rect.x + 66 + 20, (HEIGHT-menu_width_height) // 4 + 90,
+                   imgs_menu[7], imgs_menu[8], f1, '40')
+
+menu = Menu(WIDTH, HEIGHT, FPS, field1, field2, field3, but1, imgsL, XY, all_sprites, kl, bombs, num, options,
+            screen, [imgs_menu[0], imgs_menu[9], imgs_menu[10]], [imgs_menu[11], imgs_menu[12], imgs_menu[13]])
+# Создание всех элементов меню, а потом самого меню вместе с ними
+
+options.add(menu)
+options.add(but1)
+options.add(field1)
+options.add(field2)
+options.add(field3)
+
+pygame.display.set_mode((menu_width_height, menu_width_height))
+menu.render(0)
+
+status_bar = StatusBar(int(field1.text) * square_size, int(field2.text) * square_size, 0, int(field3.text),
+                       pygame.time.get_ticks(), f2)
+all_sprites.add(status_bar)
+kl['SB'] = status_bar
+
+# star the game
+size = menu.size
+# Цикл игры
+running = True
+score = 0  # количество флагов установленных на бомбах
+n_flags = 0  # количество флагов
+lose_marker = 0
+time = 0
+menu_time = 0
+
+while running:
+    clock.tick(FPS)
+    size = menu.size
+    n_bombs = int(field3.text)
+
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            running = False
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            xy = list(event.pos)
+            xy[0] = ((xy[0] - 1) // size) * size + 1
+            xy[1] = ((xy[1] - 1) // size) * size + 1
+
+            if event.button == 1:  # Левая кнопка мыши открывает клетку
+                if not num:
+                    W = int(field1.text) * size + 1
+                    H = int(field2.text) * size + 1
+                    first_click(xy[0], xy[1], XY, all_sprites, kl, bombs, num, W, H, imgsL, n_bombs, size)
+
+                if str(xy) in kl:  # Чтобы не происходило ошибок, когда игрок нажимает на статус бар
+                    if not(kl[str(xy)].chislo()):  # если попал по бомбе, то проиграл(
+                        lose_marker = -1
+
+            if event.button == 3:  # Правая кнопка мыши ставит флаг на клетку
+                if str(xy) in kl:
+                    m = kl[str(xy)].flag()
+
+                    if m[0]:  # Считаем количество флагов и сколько из них попали на бомбы
+                        score += m[1]
+                    n_flags += m[1]
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_ESCAPE:
+                start_menu_time = pygame.time.get_ticks()
+                menu.render(0)
+                stop_menu_time = pygame.time.get_ticks()
+                menu_time += stop_menu_time - start_menu_time
+
+    # Обновление
+    all_sprites.update()
+
+    if not num:  # Массив с координатми бомб пустой - маркер того, что игра началась заново, значит надо обнулить счет
+        n_flags = 0
+        score = 0
+        menu_time = 0
+
+    kl['SB'].change(pygame.time.get_ticks() - menu_time, n_flags)  # обновление значение в статус баре
+
+    screen.fill(BLACK)  # Отрисовка спрайтов
+    all_sprites.draw(screen)
+    pygame.display.flip()
+
+    if score == n_bombs:  # Проверка не выиграл ли случайно игрок
+        score = 0
+        menu.render(1)
+
+    if lose_marker == -1:  # Проверка не проиграли ли случайно игрок
+        lose_marker = 0
+        score = 0
+        menu.render(-1)
+
+
+pygame.quit()
